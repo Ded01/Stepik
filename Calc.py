026df549e631cac99b7bfc3eb28712fa63f35b60
Ivan <31912106+Shihkauskas@users.noreply.github.com>
2017-12-02 10:16:09 +0500

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,28 +1,24 @@
-a = float(input('Введите второе число: '))
-c = str(input('Введите вид операции:'))
-b = float(input('Введите второе число: '))
+a = float(input())
+b = float(input())
+c = str(input())
 
 if c == '+':
-    print('Ответ: ', a + b)
+    print(a + b)
 elif c == '-':
-    print('Ответ: ', a - b)
+    print(a - b)
 elif c == '*':
-    print('Ответ: ', a * b)
+    print(a * b)
 elif c == 'mod' and b != 0:
-    print('Ответ: ', a % b)
+    print(a % b)
 elif c == 'mod' and b == 0:
     print('Деление на 0!')
 elif c == 'pow':
-    print('Ответ: ', a ** b)
+    print(a ** b)
 elif c == 'div' and b != 0:
-    print('Ответ: ', a // b)
+    print(a // b)
 elif c == 'div' and b == 0:
-    print('Ответ: ', 'Деление на 0!')
+    print('Деление на 0!')
 elif c == '/' and b != 0:
-    print('Ответ: ', a / b)
+    print(a / b)
 elif c == '/' and b == 0:
     print('Деление на 0!')
-
-
-
-# решение задачи из курса на https://stepik.org
